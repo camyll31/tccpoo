4f96929291952a0420c18b6bb85ff90a6b528311
Manuela <manuela.belmino@academico.ifpb.edu.br>
2021-03-15 15:33:45 -0300

    Código inicial

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ let tccpoo = require("./tccpoo");
 // let Audio = require("./model/Audio");
 // let Galeria = require("./model/Galeria");
 // let Policia = require("./model/Policia");
-// let Denuncia = require("./model/Denuncia");
+let Denuncia = require("./model/Denuncia");
 
 async function sicronizar() {
     await tccpoo.sync();
