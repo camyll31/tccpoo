f3c8cdfe761bcefc31cc953a597efacec2f1263e
Manuela <manuela.belmino@academico.ifpb.edu.br>
2021-04-22 18:48:32 -0300

    Código inicial

--- src/model/DAO.js
+++ src/model/DAO.js
@@ -5,139 +5,249 @@ const Galeria = require("../model/Galeria");
 const Pessoa = require("../model/Pessoa");
 const Policia = require("../model/Policia");
 
-var Amigos = {
-    create: async function (objeto){
-        let a = await Amigos.create(objeto);
-        console.log(a);
-    },
-    ler: async function (){
-        let a = await Amigos.findAll();
-        console.log(a);
-    },
-    atualizar: async function(nome, sobrenome, telefone){
-        const usu = await Amigos.findByPk(sobrenome);
-        await usu.update({nome:`${nome}`});
-        await usu.update({telefone:`${telefone}`});
-    },
-    deletar: async function (sobrenome){
-        const object = await Amigos.findByPk(sobrenome);
-        let a = await object.destroy();
-        console.log(a);
-}
-}
-
-var Audio = {
+var amigos = {
 
     create: async function (objeto){
-    let b = await Audio.create(objeto);
-    console.log(b);
-    },
-    ler: async function (){
-        let b = await Audio.findAll();
-        console.log(b);
-        },
+try{
+    let amigos = await Amigos.create(objeto);
+    console.log(amigos);
+    }
+catch(e){
+    throw new error (name = "ERRO")
+    console.log(e.name)
+  }
+},
+        let: async function (){
+ try{
+    let amigos = await Amigos.findAll();
+
+        console.log(amigos);
+        }
+        catch (e){
+    throw new error (name= "ERRO");
+console.log(e.name);
+}
+},
+
+    atualizar: async function (atualizarNome, atualizarSobrenome, telefone, id){
+try{
+const nome = await Amigos.findByPk(id);
+        await novo.update({nome:`${atualizarNome}`});
+        await novo.update({sobrenome:`${atualizarSobrenome}`});
+        await novo.update({telefone:`${telefone}`});
+    }
+catch (e){
+     throw new error (name = "ERRO")
+     console.log(e.name)
+}
+},
+    deletar: async function (id){
+try{
+        const object = await Amigos.findByPk(id);
+        let amigos = await object.destroy();
+        console.log(amigos);
+}
+catch (e){
+ throw new error (name = "ERRO")
+ console.log(e.name)
+      }
+   }
+}
+
+var audio = {
+
+    create: async function (objeto){
+try{
+    let audio = await Audio.create(objeto);
+    console.log(audio);
+    }
+catch(e){
+    throw new error (name = "ERRO")
+    console.log(e.name)
+  }
+},
+        let: async function (){
+ try{
+    let audio = await Audio.findAll();
+
+        console.log(audio);
+        }
+        catch (e){
+    throw new error (name= "ERRO");
+console.log(e.name);
+}
+},
 
     atualizar: async function (PessoaEndereco, id){
-        const novo = await Assunto.findByPk(id);
+try{
+const nome = await Audio.findByPk(id);
         await novo.update({endereco:`${PessoaEndereco}`});
-    },
-    deletar: async function (id){
-        const object = await Audio.findByPk(id);
-        let b = await object.destroy();
-        console.log(b);
+    }
+catch (e){
+     throw new error (name = "ERRO")
+     console.log(e.name)
 }
+},
+    deletar: async function (id){
+try{
+        const objeto = await Audio.findByPk(id);
+        let audio = await objeto.destroy();
+        console.log(audio);
+}
+catch (e){
+ throw new error (name = "ERRO")
+ console.log(e.name)
+      }
+   }
 }
 
 var Denuncia = {
 
     create: async function (objeto){
-        let c = await Denuncia.create(objeto);
-        console.log(c);
-    },
-    ler: async function (){
-     let c = await Denuncia.findAll();
-     console.log(c);
-    },
+try{
+    let denuncia = await Denuncia.create(objeto);
+    console.log(denuncia);
+    }
+catch(e){
+    throw new error (name = "ERRO")
+    console.log(e.name)
+  }
+},
+        let: async function (){
+ try{
+    let denuncia = await Denuncia.findAll();
+
+        console.log(denuncia);
+        }
+        catch (e){
+    throw new error (name= "ERRO");
+console.log(e.name);
+}
+},
+
     atualizar: async function (violenciadomestica, estupro, assedio, pedofilia, id){
-        const novo = await Denuncia.findByPk(id);
-        await novo.update({violencia1:`${violenciadomestica}`});
-        await novo.update({violencia2:`${estupro}`});
-        await novo.update({violencia3:`${assedio}`});
-        await novo.update({violencia4:`${pedofilia}`});
-    },
-     deletar: async function (id){
-         const object = await Denuncia.findByPk(id);
-         let c = await object.destroy();
-         console.log(c);
-     }
+try{
+const nome = await Denuncia.findByPk(id);
+        await novo.update({violenciadomestica:`${violenciadomestica}`});
+        await novo.update({estupro:`${estupro}`});
+        await novo.update({assedio:`${assedio}`});
+        await novo.update({pedofilia:`${pedofilia}`});
+    }
+catch (e){
+     throw new error (name = "ERRO")
+     console.log(e.name)
 }
-var Galeria = {
+},
+    deletar: async function (id){
+try{
+        const objeto = await Denuncia.findByPk(id);
+        let denuncia = await objeto.destroy();
+        console.log(denuncia);
+}
+catch (e){
+ throw new error (name = "ERRO")
+ console.log(e.name)
+      }
+   }
+}
+
+var pessoa = {
 
     create: async function (objeto){
-    let d = await Galeria.create(objeto);
-    console.log(d);
-    },
-    ler: async function (){
-        let d = await Galeria.findAll();
-        console.log(d);
-        },
+try{
+    let pessoa = await Pessoa.create(objeto);
+    console.log(pessoa);
+    }
+catch(e){
+    throw new error (name = "ERRO")
+    console.log(e.name)
+  }
+},
+        let: async function (){
+ try{
+    let pessoa = await Pessoa.findAll();
 
-    atualizar: async function (foto, video, id){
-        const novo = await Galeria.findByPk(id);
-        await novo.update({foto:`${foto}`});
-        await novo.update({video:`${video}`});
-    },
+        console.log(pessoa);
+        }
+        catch (e){
+    throw new error (name= "ERRO");
+console.log(e.name);
+}
+},
+
+    atualizar: async function (Nomeatualizar, Sobrenomeatualixar, telefoneatualizar, enderecoatualizar id){
+try{
+const nome = await Pessoa.findByPk(id);
+            await novo.update({nome:`${Nomeatualizar}`});
+            await novo.update({sobrenome:`${Sobrenomeatualixar}`});
+            await novo.update({telefone:`${telefoneatualizar}`});
+            await novo.update({endereco:`${enderecoatualizar}`});
+    }
+catch (e){
+     throw new error (name = "ERRO")
+     console.log(e.name)
+}
+},
     deletar: async function (id){
-        const object = await Galeria.findByPk(id);
-        let d = await object.destroy();
-        console.log(d);
+try{
+        const objeto = await Pessoa.findByPk(id);
+        let pessoa = await objeto.destroy();
+        console.log(pessoa);
 }
+catch (e){
+ throw new error (name = "ERRO")
+ console.log(e.name)
+      }
+   }
 }
-var Pessoa = {
+
+var policia = {
 
     create: async function (objeto){
-    let e = await Pessoa.create(objeto);
-    console.log(e);
-    },
-    ler: async function (){
-        let e = await Pessoa.findAll();
-        console.log(e);
-        },
+try{
+    let policia = await Policia.create(objeto);
+    console.log(policia);
+    }
+catch(e){
+    throw new error (name = "ERRO")
+    console.log(e.name)
+  }
+},
+        let: async function (){
+ try{
+    let policia = await Policia.findAll();
 
-    atualizar: async function (nome, sobrenome, endereco, telefone, id){
-        const novo = await Pessoa.findByPk(id);
-        await novo.update({nome:`${nome}`});
-        await novo.update({sobrenome:`${sobrenome}`});
-        await novo.update({telefone:`${telefone}`});
-        await novo.update({endereco:`${endereco}`});
-    },
+        console.log(policia);
+        }
+        catch (e){
+    throw new error (name= "ERRO");
+console.log(e.name);
+}
+},
+
+    atualizar: async function (enderecoatualiza, id){
+try{
+const nome = await Policia.findByPk(id);
+            await novo.update({endereco:`${enderecoatualiza}`});
+    }
+catch (e){
+     throw new error (name = "ERRO")
+     console.log(e.name)
+}
+},
     deletar: async function (id){
-        const object = await Pessoa.findByPk(id);
-        let e = await object.destroy();
-        console.log(e);
+try{
+        const objeto = await Policia.findByPk(id);
+        let policia = await objeto.destroy();
+        console.log(policia);
 }
+catch (e){
+ throw new error (name = "ERRO")
+ console.log(e.name)
+      }
+   }
 }
-var Policia = {
 
-    create: async function (objeto){
-    let f = await Policia.create(objeto);
-    console.log(f);
-    },
-    ler: async function (){
-        let f = await Policia.findAll();
-        console.log(f);
-        },
-
-    atualizar: async function (endereco, id){
-        const novo = await Assunto.findByPk(id);
-        await novo.update({endereco:`${endereco}`});
-    },
-    deletar: async function (id){
-        const object = await Policia.findByPk(id);
-        let f = await object.destroy();
-        console.log(e);
-}
-}
 module.exports = Amigos;
 module.exports = Audio;
 module.exports = Denuncia;
